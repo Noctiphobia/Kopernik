eae64d993ab0d1ea4ae848bf89b44ddb02877eaa
Ahmed Abdelkarim <a.abdelkarim@student.mini.pw.edu.pl>
2017-03-25 15:13:57 +0100

    disabled warnings

--- data.R
+++ data.R
@@ -4,17 +4,17 @@ obserwacje = read.csv('obserwacje.csv', na.strings = c("", " ", "NA"))
 
 ###Cleaning - ankieta
 
-###Order levels
+#Order levels
 ankieta$klasyfikacja_z = ordered(ankieta$klasyfikacja_z,
-                                  levels = c("ca³kowicie niezwi¹zany z science",
-                                             "poœrednio zwi¹zany z science",
-                                             "bezpoœrdnio zwi¹zany z science"))
+                                  levels = c("ca?kowicie niezwi?zany z science",
+                                             "po?rednio zwi?zany z science",
+                                             "bezpo?rdnio zwi?zany z science"))
 time_frequency = c(21, 38:49)
 for (i in time_frequency)
 {
   ankieta[, i] = ordered(ankieta[, i],
                    levels = c("nigdy albo rzadko (raz w roku)", "kilka razy w roku",
-                              "raz lub dwa razy w miesi¹cu", "raz w tygodniu",
+                              "raz lub dwa razy w miesi?cu", "raz w tygodniu",
                               "prawie codziennie"))
 }
 
@@ -23,15 +23,15 @@ time_frequency = c(21, 38:49)
 for (i in time_frequency)
 {
   ankieta[, i] = ordered(ankieta[, i],
-                         levels = c("zdecydowanie siê nie zgadzam", "raczej siê nie zgadzam",
-                                    "ani siê zgadzam ani siê nie zgadzam", 
-                                    "raczej siê zgadzam", "zdecydowanie siê zgadzam"))
+                         levels = c("zdecydowanie si? nie zgadzam", "raczej si? nie zgadzam",
+                                    "ani si? zgadzam ani si? nie zgadzam", 
+                                    "raczej si? zgadzam", "zdecydowanie si? zgadzam"))
 }
 
 ankieta$p_16_2_4 = ordered(ankieta$p_16_2_4,
                            levels = c("nie ma ani jednej", "kilka (mniej niÅ¼ 20)",
-                                      "du¿o (od 20 do 50)", "bardzo du¿o (wiêcej ni¿ 50 do 100)",
-                                      "ca³e mnóstwo (wiêcej ni¿ 100)"))
+                                      "du?o (od 20 do 50)", "bardzo du?o (wi?cej ni? 50 do 100)",
+                                      "ca?e mn?stwo (wi?cej ni? 100)"))
 
 
 ###Cleaning - obserwacje
@@ -39,7 +39,9 @@ levels(obserwacje$eksponat) = c(levels(obserwacje$eksponat), "PRZERWA")
 obserwacje[obserwacje$kategorie==1,]$eksponat = "PRZERWA"
 obserwacje[obserwacje$eksponat=="INNE - PLANETARIUM",]$eksponat <- "PLANETARIUM"
 
+options(warn=-1)
 for (i in 13:21){
   obserwacje[,i] = as.numeric(as.character(obserwacje[,i]))
   obserwacje[,i][obserwacje[,i]<100] = NA
 }
+options(warn=0)
