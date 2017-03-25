b46f08248adc0d671c82e4a1b0263ef1194ed90c
Ahmed Abdelkarim <a.abdelkarim@student.mini.pw.edu.pl>
2017-03-25 14:01:58 +0100

    wyczyszczone

--- data.R
+++ data.R
@@ -1,3 +1,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-ankieta = read.csv('ankieta.csv')
+ankieta = read.csv('ankieta.csv', na.strings=c(""," ","NA","nie wie","nie wiem","nieczytelne pismo", "dualizm odpowiedzi","brak odpowiedzi"))
 obserwacje = read.csv('obserwacje.csv')
+
+###Cleaning - ankieta
+
+
+
+###Cleaning - obserwacje
