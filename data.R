73365ed72d1ffb7bc42936790f2454e9c6c4cfe1
Ahmed Abdelkarim <a.abdelkarim@student.mini.pw.edu.pl>
2017-04-06 21:57:46 +0200

    'Nie pamięta'

--- data.R
+++ data.R
@@ -1,5 +1,5 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-ankieta = read.csv('ankieta.csv', na.strings=c(""," ","NA","nie wie","nie wiem","nieczytelne pismo", "dualizm odpowiedzi","brak odpowiedzi"))
+ankieta = read.csv('ankieta.csv', na.strings=c(""," ","NA","nie wie","nie wiem","nieczytelne pismo", "dualizm odpowiedzi","brak odpowiedzi","nie pamięta"))
 obserwacje = read.csv('obserwacje.csv', na.strings = c("", " ", "NA"))
 
 ###Cleaning - ankieta
